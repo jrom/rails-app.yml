a47c9372aa4852c5606070f680ee3bd29308b99c
Jordi Romero <jordi@jrom.net>
2009-12-06 20:58:21 +0100

    Gitting

---
+++ README.md
@@ -0,0 +1,17 @@
+# Super simple config file Rails Template
+
+Creates an app.yml file and an initializer to load its content. That easy.
+
+When applied will ask you for the config file name, you can change it or leave the default (app).
+
+The config file name must be alphanumeric with underscores. If an invalid file name is chosen, the default will be used.
+
+## Installation
+
+In a brand new Rails app:
+
+    rails <app> -m http://github.com/jrom/railsconfig/raw/master/railsconfig.rb
+
+In a existent Rails app:
+
+    rake rails:template LOCATION=http://github.com/jrom/railsconfig/raw/master/railsconfig.rb
---
+++ railsconfig.rb
@@ -0,0 +1,26 @@
+# railsconfig.rb
+# by Jordi Romero (jrom)
+# http://github.com/jrom/railsconfig
+
+# Now, go read the README.md
+
+config_name = ask("What do you want to call the config file? (Only word characters) [example: app]")
+config_name = "app" if config_name.empty? || config_name !~ /\A\w*$\z/
+
+file "config/#{config_name.downcase}.yml", <<FILE
+production: 
+  title: Title
+
+development: 
+  title: Title [DEV]
+
+test: 
+  title: Title [TEST]
+
+FILE
+
+file "config/initializers/load_#{config_name.downcase}_config.rb", <<FILE
+#{config_name.upcase}_CONFIG = YAML.load_file("\#{Rails.root}/config/#{config_name.downcase}.yml")[Rails.env]
+FILE
+
+puts "\n\t*** Now you can use #{config_name.upcase}_CONFIG['var'] anywhere.\n\n"
